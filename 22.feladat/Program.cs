9ff31d56d75ebacc2acfb45149e4b93d7f39a836
tanulo <mate.edina.1230@dszcbaross.edu.hu>
2026-09-24 10:38:03 +0200

    22 és 23 feladat

---
+++ 22.feladat/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérek egy számot: ");
+            int szam = int.Parse(Console.ReadLine());
+            Console.WriteLine((szam%3==0)?"Osztható 3-al matradék nélkül.": "Nem osztható 3-al matradék nélkül.");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 23.feladat/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23.feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérek egy számot:  ");
+            int szam = int.Parse(Console.ReadLine());
+            Console.Write("Kérek egy másik számot:  ");
+            int szam2 = int.Parse(Console.ReadLine());
+            Console.WriteLine((szam>szam2)?szam-szam2:szam2-szam);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
