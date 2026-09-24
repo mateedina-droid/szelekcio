795519c6ec8cead8959f7ff581ce3b3097e67bd1
tanulo <tanulo@TANULOGEP-502>
2026-09-24 09:53:33 +0200

    Add project files.

---
+++ 20.feladat/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20.feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Kérek egy számot: ");
+            int szam = int.Parse(Console.ReadLine());
+            //if (szam%2 == 0)
+            //{
+            //    Console.WriteLine("Páros");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Páratlan");
+            //}
+
+            Console.WriteLine(szam%2==0?"Páros":"Páratlan");  
+
+
+            Console.ReadKey();
+        }
+    }
+}
