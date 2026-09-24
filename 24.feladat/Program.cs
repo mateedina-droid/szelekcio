84502a1177677afdd2aebec6feff116375a7089a
tanulo <mate.edina.1230@dszcbaross.edu.hu>
2026-09-24 11:02:29 +0200

    24.feladat

--- 24.feladat/Program.cs
+++ 24.feladat/Program.cs
@@ -12,8 +12,18 @@ namespace _24.feladat
         {
             Console.Write("Adj meg egy hónap számát: ");
             int szam = int.Parse(Console.ReadLine());
-            if (szam == 1 || szam == 2 )
+            if (szam == 1 || szam == 2 || szam == 12)
                 Console.WriteLine("Téli hónap");
+            else if (szam == 3 || szam == 4 || szam == 5)
+                Console.WriteLine("Tavaszi hónap");
+            else if (szam == 6 || szam == 7 || szam == 8)
+                Console.WriteLine("Nyári hónap");
+            else if (szam == 9 || szam == 10 || szam == 11) 
+                Console.WriteLine("Őszi hónap");
+            else
+                Console.WriteLine("Nincs ilyen hónap");
+
+            Console.ReadKey();
 
         }
     }
