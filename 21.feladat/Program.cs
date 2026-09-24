896e480000f9617ac7be2407f42b420df6d421fb
tanulo <mate.edina.1230@dszcbaross.edu.hu>
2026-09-24 10:04:58 +0200

    21.feladat

---
+++ 21.feladat/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérek egy számot: ");
+            int szam = int.Parse(Console.ReadLine());
+            if (szam % 2 == 0)
+            {
+                Console.WriteLine("Páros");
+            }
+            else if (szam % 2 == 1)
+            {
+                Console.WriteLine("Páratlan");
+            }
+            else
+            {
+                Console.WriteLine("nulla");
+            }
+            Console.ReadKey();
+        }
+    }
+}
